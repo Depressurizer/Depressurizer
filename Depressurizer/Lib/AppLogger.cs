5f23a1e6ada4e916290b68c2919b23b92f1e30af
rallion <rallion@gmail.com>
2014-10-11 15:28:21 -0400

    license compliance

--- Depressurizer/Lib/AppLogger.cs
+++ Depressurizer/Lib/AppLogger.cs
@@ -1,7 +1,6 @@
 ﻿/*
-Copyright 2011, 2012, 2013 Steve Labbe.
-
 This file is part of Depressurizer.
+Copyright 2011, 2012, 2013 Steve Labbe.
 
 Depressurizer is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
