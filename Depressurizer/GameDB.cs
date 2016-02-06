7eec256d3c937be56589cc4ba046fb80ca1a7fda
Theo <theo1001@gmail.com>
2016-02-06 20:37:57 +0200

    Prevent updating from AppInfo from overwriting fields using null data

--- Depressurizer/GameDB.cs
+++ Depressurizer/GameDB.cs
@@ -838,11 +838,10 @@ namespace Depressurizer
                 }
 
                 entry.LastAppInfoUpdate = timestamp;
-                entry.AppType = aInf.AppType;
-                entry.Name = aInf.Name;
-                entry.Platforms = aInf.Platforms;
-                entry.ParentId = aInf.Parent;
-                updated++;
+                if (aInf.AppType != AppTypes.Unknown) entry.AppType = aInf.AppType;
+                if (!string.IsNullOrEmpty(aInf.Name)) entry.Name = aInf.Name;
+                if (aInf.Platforms > AppPlatforms.None) entry.Platforms = aInf.Platforms;
+                if (aInf.Parent > 0) entry.ParentId = aInf.Parent;
             }
             return updated;
         }
