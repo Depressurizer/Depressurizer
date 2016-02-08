f690c9e178bdd26ca16040a5300e2bb591bd2698
EnemyWithin <enemywithin@hargraveusa.com>
2016-02-08 07:56:49 -0500

    Added game banner grab thread.

---
+++ Depressurizer/GameBanners.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Depressurizer
+{
+    class GameBanners
+    {
+        private volatile bool _shouldStop;
+        private List<GameInfo> _games;
+
+        public GameBanners(List<GameInfo> games)
+        {
+            this._games = games;
+        }
+
+        public void Grab()
+        {
+            foreach (GameInfo g in _games)
+            {
+                if (_shouldStop)
+                {
+                    return;
+                }
+                else
+                {
+                    string bannerFile = string.Format(Properties.Resources.GameBannerPath, Path.GetDirectoryName(Application.ExecutablePath), g.Id.ToString());
+                    if (!File.Exists(bannerFile))
+                    {
+                        Utility.GrabBanner(g.Id);
+                    }
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            _shouldStop = true;
+        }
+    }
+}
