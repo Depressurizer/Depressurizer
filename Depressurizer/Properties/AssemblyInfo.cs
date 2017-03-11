4a894f19e401fe029b47dbfaf836de1d63672de7
unknown <theo1001@gmail.com>
2017-03-11 16:40:06 +0200

    Updated version to v0.7.2.0

--- Depressurizer/Properties/AssemblyInfo.cs
+++ Depressurizer/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
 [assembly: AssemblyProduct( "Depressurizer" )]
-[assembly: AssemblyCopyright("Copyright © 2011–2016")]
+[assembly: AssemblyCopyright("Copyright © 2011–2017")]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
@@ -50,5 +50,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.1.4")]
-[assembly: AssemblyFileVersion("0.7.1.4")]
+[assembly: AssemblyVersion("0.7.2.0")]
+[assembly: AssemblyFileVersion("0.7.2.0")]
