006e37ca6647f2f29c77ae88ddf880724ec8f0c0
Martijn Vegter <mvegter.coding@gmail.com>
2017-07-03 16:49:36 +0200

    Fixed AutoCatName.cs

--- Depressurizer/AutoCat/AutoCatName.cs
+++ Depressurizer/AutoCat/AutoCatName.cs
@@ -17,37 +17,27 @@
     along with Depressurizer.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-using Rallion;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
+using Rallion;
 
 namespace Depressurizer
 {
-    class AutoCatName : AutoCat
+    internal class AutoCatName : AutoCat
     {
         public string Prefix { get; set; }
         public bool SkipThe { get; set; }
         public bool GroupNumbers { get; set; }
 
-        public override AutoCatType AutoCatType
-        {
-            get
-            {
-                return AutoCatType.Name;
-            }
-        }
+        public override AutoCatType AutoCatType => AutoCatType.Name;
 
         public const string TypeIdString = "AutoCatName";
-        public const string XmlName_Prefix = "Prefix";
-        public const string XmlName_Name = "Name";
-        public const string XmlName_SkipThe = "SkipThe";
-        public const string XmlName_GroupNumbers = "GroupNumbers";
+        public const string XmlNamePrefix = "Prefix";
+        public const string XmlNameName = "Name";
+        public const string XmlNameSkipThe = "SkipThe";
+        public const string XmlNameGroupNumbers = "GroupNumbers";
 
-        public AutoCatName(string name, string prefix="",bool skipThe = true, bool groupNumbers = false):base(name)
+        public AutoCatName(string name, string prefix = "", bool skipThe = true, bool groupNumbers = false) : base(name)
         {
             Name = name;
             Prefix = prefix;
@@ -62,56 +52,66 @@ namespace Depressurizer
                 Program.Logger.Write(LoggerLevel.Error, GlobalStrings.Log_AutoCat_GamelistNull);
                 throw new ApplicationException(GlobalStrings.AutoCatGenre_Exception_NoGameList);
             }
+
             if (Db == null)
             {
                 Program.Logger.Write(LoggerLevel.Error, GlobalStrings.Log_AutoCat_DBNull);
                 throw new ApplicationException(GlobalStrings.AutoCatGenre_Exception_NoGameDB);
             }
+
             if (game == null)
             {
                 Program.Logger.Write(LoggerLevel.Error, GlobalStrings.Log_AutoCat_GameNull);
                 return AutoCatResult.Failure;
             }
 
-            if (!Db.Contains(game.Id)) return AutoCatResult.NotInDatabase;
+            if (!Db.Contains(game.Id))
+            {
+                return AutoCatResult.NotInDatabase;
+            }
 
-            
             string cat = game.Name.Substring(0, 1);
             cat = cat.ToUpper();
-            if (SkipThe && cat == "T" && game.Name.Substring(0, 4).ToUpper() == "THE ") cat = game.Name.Substring(4, 1).ToUpper();
-            if (GroupNumbers && Char.IsDigit(cat[0])) cat = "#";
-            if (Prefix!=null) cat = Prefix + cat;
+            if (SkipThe && (cat == "T") && (game.Name.Substring(0, 4).ToUpper() == "THE "))
+            {
+                cat = game.Name.Substring(4, 1).ToUpper();
+            }
+            if (GroupNumbers && char.IsDigit(cat[0]))
+            {
+                cat = "#";
+            }
+            if (Prefix != null)
+            {
+                cat = Prefix + cat;
+            }
 
             game.AddCategory(Games.GetCategory(cat));
 
             return AutoCatResult.Success;
         }
 
-        public override AutoCat Clone()
-        {
-            return new AutoCatName(Name,Prefix,SkipThe, GroupNumbers);
-        }
+        public override AutoCat Clone() => new AutoCatName(Name, Prefix, SkipThe, GroupNumbers);
 
         public override void WriteToXml(XmlWriter writer)
         {
             writer.WriteStartElement(TypeIdString);
 
-            writer.WriteElementString(XmlName_Name, Name);
-            writer.WriteElementString(XmlName_Prefix, Prefix);
-            writer.WriteElementString(XmlName_SkipThe, SkipThe.ToString());
-            writer.WriteElementString(XmlName_GroupNumbers, GroupNumbers.ToString());
+            writer.WriteElementString(XmlNameName, Name);
+            writer.WriteElementString(XmlNamePrefix, Prefix);
+            writer.WriteElementString(XmlNameSkipThe, SkipThe.ToString());
+            writer.WriteElementString(XmlNameGroupNumbers, GroupNumbers.ToString());
 
             writer.WriteEndElement(); // type ID string
         }
 
         public static AutoCatName LoadFromXmlElement(XmlElement xElement)
         {
-            string name = XmlUtil.GetStringFromNode(xElement[XmlName_Name], null);
-            string prefix = XmlUtil.GetStringFromNode(xElement[XmlName_Prefix], null);
-            bool skipThe = Boolean.Parse(XmlUtil.GetStringFromNode(xElement[XmlName_SkipThe], null));
-            bool groupNumbers = Boolean.Parse(XmlUtil.GetStringFromNode(xElement[XmlName_GroupNumbers], null));
+            string name = XmlUtil.GetStringFromNode(xElement[XmlNameName], null);
+            string prefix = XmlUtil.GetStringFromNode(xElement[XmlNamePrefix], null);
+            bool skipThe = bool.Parse(XmlUtil.GetStringFromNode(xElement[XmlNameSkipThe], null));
+            bool groupNumbers = bool.Parse(XmlUtil.GetStringFromNode(xElement[XmlNameGroupNumbers], null));
 
-            return new AutoCatName(name, prefix,skipThe,groupNumbers);
+            return new AutoCatName(name, prefix, skipThe, groupNumbers);
         }
     }
 }
