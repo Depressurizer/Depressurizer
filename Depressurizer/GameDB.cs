7e5934a8374bbb4aa087afe2286a0bfcaa6148f4
Martijn Vegter <martijn@martijnvegter.com>
2017-07-21 22:20:23 +0200

    Moved AppInfo to Model-Namespace

--- Depressurizer/GameDB.cs
+++ Depressurizer/GameDB.cs
@@ -1,19 +1,20 @@
 ﻿/*
-This file is part of Depressurizer.
-Copyright 2011, 2012, 2013 Steve Labbe.
+    This file is part of Depressurizer.
+    Original work Copyright 2011, 2012, 2013 Steve Labbe.
+    Modified work Copyright 2017 Martijn Vegter.
 
-Depressurizer is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+    Depressurizer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-Depressurizer is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+    Depressurizer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with Depressurizer.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with Depressurizer.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 using System;
@@ -26,6 +27,7 @@ using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Xml;
+using Depressurizer.Model;
 using Depressurizer.Properties;
 using Newtonsoft.Json.Linq;
 using Rallion;
@@ -1402,7 +1404,7 @@ namespace Depressurizer
         {
             int updated = 0;
 
-            Dictionary<int, AppInfo> appInfos = AppInfo.LoadApps(path);
+            Dictionary<int, AppInfo> appInfos = LoadApps(path);
             int timestamp = Utility.GetCurrentUTime();
 
             foreach (AppInfo aInf in appInfos.Values)
@@ -1429,6 +1431,120 @@ namespace Depressurizer
                 updated++;
             }
             return updated;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public Dictionary<int, AppInfo> LoadApps(string path)
+        {
+            Dictionary<int, AppInfo> result = new Dictionary<int, AppInfo>();
+            BinaryReader bReader = new BinaryReader(new FileStream(path, FileMode.Open, FileAccess.Read));
+            long fileLength = bReader.BaseStream.Length;
+
+            // seek to common: start of a new entry
+            byte[] start = { 0x00, 0x00, 0x63, 0x6F, 0x6D, 0x6D, 0x6F, 0x6E, 0x00 }; // 0x00 0x00 c o m m o n 0x00
+
+            VdfFileNode.ReadBin_SeekTo(bReader, start, fileLength);
+
+            VdfFileNode node = VdfFileNode.LoadFromBinary(bReader, fileLength);
+            while (node != null)
+            {
+                AppInfo app = FromVdfNode(node);
+                if (app != null)
+                {
+                    result.Add(app.Id, app);
+                }
+                VdfFileNode.ReadBin_SeekTo(bReader, start, fileLength);
+                node = VdfFileNode.LoadFromBinary(bReader, fileLength);
+            }
+            bReader.Close();
+            return result;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="commonNode"></param>
+        /// <returns></returns>
+        /// TODO Constructor in AppInfo.cs
+        public AppInfo FromVdfNode(VdfFileNode commonNode)
+        {
+            if (commonNode == null || commonNode.NodeType != ValueType.Array) return null;
+
+            AppInfo result = null;
+
+            VdfFileNode idNode = commonNode.GetNodeAt(new[] { "gameid" }, false);
+            int id = -1;
+            if (idNode != null)
+            {
+                if (idNode.NodeType == ValueType.Int)
+                {
+                    id = idNode.NodeInt;
+                }
+                else if (idNode.NodeType == ValueType.String)
+                {
+                    if (!int.TryParse(idNode.NodeString, out id))
+                    {
+                        id = -1;
+                    }
+                }
+            }
+
+            if (id >= 0)
+            {
+                // Get name
+                string name = null;
+                VdfFileNode nameNode = commonNode.GetNodeAt(new[] { "name" }, false);
+                if (nameNode != null) name = nameNode.NodeData.ToString();
+
+                // Get type
+                string typeStr = null;
+                AppTypes type = AppTypes.Unknown;
+                VdfFileNode typeNode = commonNode.GetNodeAt(new[] { "type" }, false);
+                if (typeNode != null) typeStr = typeNode.NodeData.ToString();
+
+                if (typeStr != null)
+                {
+                    if (!Enum.TryParse(typeStr, true, out type))
+                    {
+                        type = AppTypes.Other;
+                    }
+                }
+
+                // Get platforms
+                string oslist = null;
+                AppPlatforms platforms = AppPlatforms.None;
+                VdfFileNode oslistNode = commonNode.GetNodeAt(new[] { "oslist" }, false);
+                if (oslistNode != null)
+                {
+                    oslist = oslistNode.NodeData.ToString();
+                    if (oslist.IndexOf("windows", StringComparison.OrdinalIgnoreCase) != -1)
+                    {
+                        platforms |= AppPlatforms.Windows;
+                    }
+                    if (oslist.IndexOf("mac", StringComparison.OrdinalIgnoreCase) != -1)
+                    {
+                        platforms |= AppPlatforms.Mac;
+                    }
+                    if (oslist.IndexOf("linux", StringComparison.OrdinalIgnoreCase) != -1)
+                    {
+                        platforms |= AppPlatforms.Linux;
+                    }
+                }
+
+                result = new AppInfo(id, name, type, platforms);
+
+                // Get parent
+                VdfFileNode parentNode = commonNode.GetNodeAt(new[] { "parent" }, false);
+                if (parentNode != null)
+                {
+                    result.Parent = parentNode.NodeInt;
+                }
+            }
+            return result;
         }
 
         /// <summary>
