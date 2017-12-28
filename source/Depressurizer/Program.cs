7d8aa9486bee40b37817824f45a6cfbb709894e9
Martijn Vegter <mail@mvegter.me>
2017-12-28 14:26:04 +0100

    Code Style

--- source/Depressurizer/Program.cs
+++ source/Depressurizer/Program.cs
@@ -1,44 +1,57 @@
-﻿/*
-This file is part of Depressurizer.
-Copyright 2011, 2012, 2013 Steve Labbe.
+﻿#region LICENSE
 
-Depressurizer is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+//     This file (Program.cs) is part of Depressurizer.
+//     Copyright (C) 2017  Martijn Vegter
+// 
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-Depressurizer is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Depressurizer.  If not, see <http://www.gnu.org/licenses/>.
-*/
+#endregion
 
 using System;
 using System.Windows.Forms;
-using Rallion;
 
 namespace Depressurizer
 {
-    static class Program
+    internal static class Program
     {
+        #region Static Fields
+
         public static GameDB GameDB;
 
+        #endregion
+
+        #region Methods
+
         /// <summary>
-        /// The main entry point for the application.
+        ///     The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.ApplicationExit += OnApplicationExit;
 
             Settings.Instance.Load();
             Application.Run(new FormMain());
-            
+        }
+
+        private static void OnApplicationExit(object sender, EventArgs eventArgs)
+        {
             Settings.Instance.Save();
         }
+
+        #endregion
     }
 }
