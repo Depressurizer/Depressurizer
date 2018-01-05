5d1a87211b427e7e12b82497bc7696ef671debd2
Martijn Vegter <mail@mvegter.me>
2018-01-05 18:38:59 +0100

    Category Model

--- src/Depressurizer/GameInfo.cs
+++ src/Depressurizer/GameInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using DepressurizerCore.Models;
 
 /*
 This file is part of Depressurizer.
--- src/Depressurizer/GameList.cs
+++ src/Depressurizer/GameList.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Net;
 using System.Text.RegularExpressions;
 using System.Xml;
+using DepressurizerCore.Models;
 using Rallion;
 
 namespace Depressurizer
