32e50a980a8d2d5528bd0fdef9ae2e8f9c9d6f3a
Martijn Vegter <martijn@martijnvegter.com>
2018-02-26 11:14:34 +0100

    ProfileAccessException Model

--- Source/Depressurizer/GameData.cs
+++ Source/Depressurizer/GameData.cs
@@ -1998,15 +1998,4 @@ namespace Depressurizer
 
 		#endregion
 	}
-
-	internal class ProfileAccessException : ApplicationException
-	{
-		#region Constructors and Destructors
-
-		public ProfileAccessException(string m) : base(m)
-		{
-		}
-
-		#endregion
-	}
 }
