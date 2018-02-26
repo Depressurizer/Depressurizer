569faec5859c86b531cb24e29e19150efcacf9e5
Martijn Vegter <martijn@martijnvegter.com>
2018-02-26 18:00:30 +0100

    Cleanup Program

--- Source/Depressurizer/Program.cs
+++ Source/Depressurizer/Program.cs
@@ -29,11 +29,8 @@ namespace Depressurizer
 	{
 		#region Methods
 
-		/// <summary>
-		///     The main entry point for the application.
-		/// </summary>
 		[STAThread]
-		private static void Main(string[] args)
+		private static void Main()
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
@@ -53,7 +50,6 @@ namespace Depressurizer
 			}
 
 			Settings.Instance.Save();
-
 			Logger.Instance.Dispose();
 		}
 
