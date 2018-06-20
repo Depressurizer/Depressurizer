92b212fb303ff2d841d13d078db0e1a1b8b277b5
Martijn Vegter <martijn@martijnvegter.com>
2018-06-20 10:36:24 +0200

    Revert "Reworked program.cs"
    This reverts commit a61228eb46eed7b3be18833dcf4f56ecf327a94e.

---
+++ src/Depressurizer/AutomaticModeForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Depressurizer {
+    partial class AutomaticModeForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing ) {
+            if( disposing && ( components != null ) ) {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AutomaticModeForm));
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.cmdClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(7, 7);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(706, 299);
+            this.txtOutput.TabIndex = 0;
+            // 
+            // cmdClose
+            // 
+            this.cmdClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cmdClose.Location = new System.Drawing.Point(7, 307);
+            this.cmdClose.Name = "cmdClose";
+            this.cmdClose.Size = new System.Drawing.Size(706, 23);
+            this.cmdClose.TabIndex = 1;
+            this.cmdClose.Text = "Close";
+            this.cmdClose.UseVisualStyleBackColor = true;
+            this.cmdClose.Click += new System.EventHandler(this.cmdClose_Click);
+            // 
+            // AutomaticModeForm
+            // 
+            this.AcceptButton = this.cmdClose;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cmdClose;
+            this.ClientSize = new System.Drawing.Size(720, 336);
+            this.ControlBox = false;
+            this.Controls.Add(this.cmdClose);
+            this.Controls.Add(this.txtOutput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AutomaticModeForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Depressurizer Automatic Mode";
+            this.Load += new System.EventHandler(this.AutomaticModeForm_Load);
+            this.Shown += new System.EventHandler(this.AutomaticModeForm_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button cmdClose;
+    }
+}
