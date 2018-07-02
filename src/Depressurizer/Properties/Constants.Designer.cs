a6569da2cb5198dacb96d1327c7ba3ea7a6100fb
Martijn Vegter <martijn@martijnvegter.com>
2018-07-02 19:12:51 +0200

    Moved Steam helper to Core

--- src/Depressurizer/Properties/Constants.Designer.cs
+++ src/Depressurizer/Properties/Constants.Designer.cs
@@ -214,15 +214,6 @@ namespace Depressurizer.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to https://steamcdn-a.akamaihd.net/steam/apps/{0}/capsule_sm_120.jpg.
-        /// </summary>
-        internal static string SteamStoreAppBanner {
-            get {
-                return ResourceManager.GetString("SteamStoreAppBanner", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to http://store.steampowered.com/curators/ajaxgetcuratorrecommendations/{0}/?query=&amp;start={1}&amp;count=50.
         /// </summary>
         internal static string UrlSteamCuratorRecommendations {
