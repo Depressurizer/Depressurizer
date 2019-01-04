6e19a02d63f3a785692144a191a72eae7230b900
Martijn Vegter <martijn@martijnvegter.com>
2019-01-04 19:53:18 +0100

    ✨ Added unit test project (#106)

---
+++ src/Depressurizer.Tests/Enums/InterfaceLanguageTests.cs
@@ -0,0 +1,23 @@
+﻿using Depressurizer.Core.Enums;
+using FluentAssertions;
+using Xunit;
+
+namespace Depressurizer.Tests.Enums
+{
+    public class InterfaceLanguageTests
+    {
+        #region Public Methods and Operators
+
+        [Fact]
+        public void Values()
+        {
+            InterfaceLanguage.English.Should().Be(0);
+            InterfaceLanguage.Spanish.Should().Be(1);
+            InterfaceLanguage.Russian.Should().Be(2);
+            InterfaceLanguage.Ukrainian.Should().Be(3);
+            InterfaceLanguage.Dutch.Should().Be(4);
+        }
+
+        #endregion
+    }
+}
