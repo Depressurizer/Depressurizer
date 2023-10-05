12ffe563a014f6b149f3a5e40f9c6a409969a0be
Brad Russell <brad.russell@e-gineering.com>
2023-10-05 14:11:01 -0400

    Rename interface parameters to match implementations

--- src/Depressurizer.Core/Interfaces/IGameList.cs
+++ src/Depressurizer.Core/Interfaces/IGameList.cs
@@ -25,7 +25,7 @@ namespace Depressurizer.Core.Interfaces
 
         #region Public Methods and Operators
 
-        Category AddCategory(string dialogValue, bool forModification = false);
+        Category AddCategory(string name, bool forRename = false);
 
         Filter AddFilter(string name);
 
@@ -33,7 +33,7 @@ namespace Depressurizer.Core.Interfaces
 
         void AddGameCategory(int[] appIds, Category category);
 
-        bool CategoryExists(string prefix, StringComparison stringComparison = StringComparison.OrdinalIgnoreCase);
+        bool CategoryExists(string name, StringComparison stringComparison = StringComparison.OrdinalIgnoreCase);
 
         void ClearGameCategories(int appId, bool preserveFavorite);
 
