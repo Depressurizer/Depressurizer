d5f9639a934cc90f33d988b302e58169c8cd868b
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2025-06-05 00:29:25 +0200

    Depressurizer.Core+Tests: Add missing AssemblyInfo.cs
    Also cleanup unused using lines

---
+++ src/Depressurizer.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿/*
+  This source is subject to the Microsoft Public License. See LICENSE.TXT for details.
+  The original developer is Iros <irosff@outlook.com>
+*/
+
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Depressurizer.Core")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Depressurizer.Core")]
+[assembly: AssemblyCopyright("Julian Xhokaxhiu © 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.0")]
